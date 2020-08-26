5f037b4a79c36271bb48286314dae9fd855c3450
Guh.Snt.Lean <guleandro.66@gmail.com>
2020-08-25 22:09:52 -0300

    Recreate project

--- Config/DB/Connection.php
+++ Config/DB/Connection.php
@@ -1,27 +1,21 @@
 <?
-  // TODO: Melhorar algumas coisas da conexao com o banco de dados de forma que de para criar e verificar se o banco e exestento por comandos
-
   use PDO;
 
-  class Connection {
-    private static $user      = DBUSER;
-    private static $password  = DBPASSWORD;
-    private static $domain    = DBDOMAIN;
-    private static $instance  = null;
-    
+  class Connection{
+    private static $instance = null;
+
     private function __construct(){}
     private function __clone(){}
 
     public static function getInstance(){
       try{
         if(is_null(self::$instance)){
-          self::$instance = new PDO(self::$dns, self::$user, self::$password);
-          self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          self::$instance = new PDO();
+          self::$instance->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         }
         return self::$instance;
       }catch(Exception $e){
-        return ($e->getMessage());
+        echo $e;
       }
     }
   }
-
--- Config/Route/Route.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-  $rotas = [];
-
-  return $rotas;
