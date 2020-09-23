a4a4d7f35a6da8de9249ff6787502257aa759091
Guh.Snt.Lean <guleandro.66@gmail.com>
2020-09-23 04:59:04 -0300

    Implementacao inicial do controller

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ require_once ('autoload.php');
 
 use \Source\Controller\Controller;
 
-// var_dump(new \Source\Controller\Controller());
-
 $app = new Controller();
 
-// $app->setRoutes(require('../route/routes.php'));
-// $app->run();
+$app->run();
